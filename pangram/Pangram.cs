a7fb0425d493db1c628680a0c0aadedd7d25201e
Anthony Scinocco <scinocco.a@gmail.com>
2018-08-13 18:48:16 -0400

    Damn you visual studio. Locking up my files. Finished pangram.

--- pangram/Pangram.cs
+++ pangram/Pangram.cs
@@ -3,66 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-
-// @TODO: Anthony - Check the tests, there are certain edge cases you need to handle
-// underscores, numbers, etc
 public static class Pangram
 {
-    private static readonly char[] Alphabet = new char[]
-    {
-        'a','b','c','d','e','f','g','h','i','j','k','l','m',
-        'n','o','p','q','r','s','t','u','v','w','x','y','z'
-    };
+    private static readonly int ALPHABET_LETTER_COUNT = 26;
 
-    private static char[] filterInput (char[] inputChars)
+    public static bool IsPangram(string input)
     {
-      var filteredInputChars = inputChars.Where(
-        x => !string.IsNullOrEmpty(x.ToString()) && x.GetType() == typeof(char)
-      );
-      return filteredInputChars.ToArray<char>();
+        string parsedInput = getParsedString(input);
+        return parsedInput.Length >= ALPHABET_LETTER_COUNT;
+    }
+    public static string getParsedString(string input)
+    {
+        string alphaString = stripNonAlphaCharacters(input.ToLower());
+        return removeDuplicateLetters(alphaString);
     }
 
-    // @TODO: Anthony - setup regex to strip all disallowed characters. 
-  public static bool IsPangram(string input)
-  {
-    string parsedInput = getStrippedString(input);
-    var inputChars = input.ToLower().ToArray();
-    var filteredInputChars = filterInput(inputChars);
-        if (filteredInputChars.Length < Alphabet.Length) return false;
-        List<char> foundLetters = new List<char>();
-        foreach (char inputChar in filteredInputChars)
+    public static string stripNonAlphaCharacters(string input)
+    {
+        Regex stripCharsRgx = new Regex("[^a-z]");
+        return stripCharsRgx.Replace(input, "");
+    }
+
+    public static string removeDuplicateLetters(string input)
+    {
+        string lowerCaseInput = input.ToLower();
+        string result = "";
+        foreach(char value in lowerCaseInput)
         {
-            char lowerCaseChar = char.ToLower(inputChar);
-            var found = foundLetters.Contains(lowerCaseChar);
-            if (!found) foundLetters.Add(lowerCaseChar);
+            if (result.IndexOf(value) == -1)
+            {
+                result += value;
+            }
         }
-        return foundLetters.Count == Alphabet.Length;
+
+        return result;
     }
-
-  public static string getStrippedString(string input)
-  {
-    string alphaString = stripNonAlphaCharacters(input.ToLower());
-    return removeDuplicateLetters(alphaString);
-  }
-
-  public static string stripNonAlphaCharacters(string input)
-  {
-    Regex stripCharsRgx = new Regex("[^a-z]");
-    return stripCharsRgx.Replace(input, "");
-  }
-
-  public static string removeDuplicateLetters(string input)
-  {
-    string lowerCaseInput = input.ToLower();
-    string result = "";
-    foreach(char value in lowerCaseInput)
-    {
-      if (result.IndexOf(value) == -1)
-      {
-        result += value;
-      }
-    }
-
-    return result;
-  }
 }
