fdfddbaef84cfe2f36b8271532c24fb3f8c5057c
Anthony Scinocco <scinocco.a@gmail.com>
2018-08-16 22:57:15 -0400

    1 failing test remains on leap year

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -2,8 +2,19 @@ using System;
 
 public static class Leap
 {
+
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (year % 4 == 0 && year % 100 != 0)
+        {
+            return true;
+        }
+
+        if (year % 4 == 0 && year % 100 != 0 && year % 400 == 0)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
